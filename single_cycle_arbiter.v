1f1324c08a90f834a2077a149fd18e33c9fa51b6
Cb_cat <leapofleopard@gmail.com>
2025-09-20 16:16:26 +0530

    commit 4: Tried alternate approach, but Variable Part Select is not supported by SV. Slight modificaiton to the IF statement fixes the time consumed by synthesizer.

--- single_cycle_arbiter.v
+++ single_cycle_arbiter.v
@@ -9,7 +9,9 @@ module single_cycle_arbiter #(
 
     // Write your logic here...
     integer i;
+    reg         ls_one;
     reg [N-1:0] dout;
+    
     
     assign gnt_o = dout;
     
@@ -17,26 +19,42 @@ module single_cycle_arbiter #(
         if (N == 1) begin : generate_block_1
             always_latch begin 
                 if (reset) begin 
+                    ls_one = 0;
                     dout   = 0;
                 end else if (clk) begin 
+                    ls_one = ls_one;
                     dout   = req_i;
                 end else begin 
+                    ls_one = ls_one;
                     dout   = dout;
                 end 
             end 
         end else begin : generate_block_2
             always_latch begin 
                 if (reset) begin 
+                    ls_one = 0;
                     dout   = 0;
                 end else if (clk) begin 
-                    dout  = 0;
+                    ls_one  = 0;
+                //  dout[0] = req_i[0];
+                    dout    = 0;
                     for (i = 0; i <= N-1; i=i+1) begin 
-                        if (req_i[i] == 1'b1) begin 
-                            dout[i] = 1'b1;
-                            break;
+                        if (req_i[i] & (~ls_one)) begin 
+                            ls_one  = 1;
+                            dout[i] = 1;
                         end 
+                        /*
+                        if (req_i[i]) begin 
+                        // if all the preceeding bits are "0", 
+                        // this is the first occurance of "1",
+                        // hence it should be "1", else "0".
+                        // Unary reduction NOR operator.
+                            dout[i] = ~|(req_i[(i-1)-:]) ? 1 : 0;
+                        end 
+                        */
                     end 
                 end else begin 
+                    ls_one = ls_one;
                     dout   = dout;
                 end
             end 
